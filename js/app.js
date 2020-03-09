5600122eb63e44daa8dd01286c31a3a237347b92
Kristoffer Alquiza <kalquiza@gmail.com>
2020-03-08 18:35:07 -0700

    Refactor udacity standard style

--- js/app.js
+++ js/app.js
@@ -1,123 +1,123 @@
+/* eslint-disable semi */
 /**
- * 
+ *
  * Manipulating the DOM exercise.
  * Exercise programmatically builds navigation,
  * scrolls to anchors from navigation,
  * and highlights section in viewport upon scrolling.
- * 
+ *
  * Dependencies: None
- * 
+ *
  * JS Version: ES2015/ES6
- * 
+ *
  * JS Standard: ESlint
- * 
+ *
 */
 
 /**
  * Define Global Variables
- * 
+ *
 */
-let sections = [];
+const sections = [];
 
 /**
  * End Global Variables
  * Start Helper Functions
- * 
+ *
 */
 
-// Get the page sections and store them in the sections array 
-function getSections() {
-    let elem = {};
-    let i = 1;
-    do {
-        elem = document.getElementById(`section${i}`);
-        if (elem != null) sections.push(elem);
-        i++;
-    } while (elem != null)
+// Get the page sections and store them in the sections array
+const getSections = () => {
+  let elem = {};
+  let i = 1;
+  do {
+    elem = document.getElementById(`section${i}`);
+    if (elem != null) sections.push(elem);
+    i++;
+  } while (elem != null)
 }
 
 /**
  * End Helper Functions
  * Begin Main Functions
- * 
+ *
 */
 
 // Build the navigation bar
-function buildNav() {
-    // select the navbar__list
-    const nav = document.querySelector("#navbar__list");
-    // for each section
-    for (const section of sections) {
-        // build a new <li> element with appropriate innerHTML and styled class
-        const newSection = document.createElement('li');
-        newSection.textContent = section.getAttribute("data-nav");
-        newSection.classList.add('menu__link');
+const buildNav = () => {
+  // select the navbar__list
+  const nav = document.querySelector('#navbar__list');
+  // for each section
+  for (const section of sections) {
+    // build a new <li> element with appropriate innerHTML and styled class
+    const newSection = document.createElement('li');
+    newSection.textContent = section.getAttribute('data-nav');
+    newSection.classList.add('menu__link');
 
-        newSection.id = `${section.id}-menulink`;
-        // add the the <li> element as the last child element of navbar__list
-        nav.appendChild(newSection);
-    }
+    newSection.id = `${section.id}-menulink`;
+    // add the the <li> element as the last child element of navbar__list
+    nav.appendChild(newSection);
+  }
 }
 
 // Add class 'active' to section when near top of viewport
-function setActive() {
-    // get the height of the viewport and navbar
-    const viewportHeight = document.documentElement.clientHeight;
-    const navbarHeight = document.querySelector(".navbar__menu").getBoundingClientRect().height;
+const setActive = () => {
+  // get the height of the viewport and navbar
+  const viewportHeight = document.documentElement.clientHeight;
+  const navbarHeight = document.querySelector('.navbar__menu').getBoundingClientRect().height;
 
-    for (const section of sections) {
-        // get the top boundary and height of the section
-        sectionContainerTop = section.getBoundingClientRect().top;
-        sectionContainerHeight = section.getBoundingClientRect().height;
+  for (const section of sections) {
+    // get the top boundary and height of the section
+    const sectionContainerTop = section.getBoundingClientRect().top;
+    const sectionContainerHeight = section.getBoundingClientRect().height;
 
-        // determine active viewport and set or remove the active class
-        if (
-            sectionContainerTop + sectionContainerHeight > navbarHeight + (viewportHeight-navbarHeight)/2
-            && sectionContainerTop < navbarHeight + (viewportHeight-navbarHeight)/2
-            )
-        {
-            section.classList.add("your-active-class");
-        } else {
-            section.classList.remove("your-active-class");
-        }
+    // determine active viewport and set or remove the active class
+    if (
+      sectionContainerTop + sectionContainerHeight > navbarHeight + (viewportHeight - navbarHeight) / 2 &&
+            sectionContainerTop < navbarHeight + (viewportHeight - navbarHeight) / 2
+    ) {
+      section.classList.add('your-active-class');
+    } else {
+      section.classList.remove('your-active-class');
     }
+  }
 }
 
 /**
  * End Main Functions
  * Begin Events
- * 
+ *
 */
 
 // Get sections
 getSections();
-// Build menu 
+// Build menu
 buildNav();
 // Determine initial active section
 setActive();
 
 /**
  * Add Event Listeners
- * 
+ *
 */
 // Scroll to section on menu link click
 for (const section of sections) {
-    const menulink = document.querySelector(`#${section.id}-menulink`);
-    const navbarHeight = document.querySelector(".navbar__menu").getBoundingClientRect().height;
+  const menulink = document.querySelector(`#${section.id}-menulink`);
+  const navbarHeight = document.querySelector('.navbar__menu').getBoundingClientRect().height;
 
-    menulink.addEventListener('click', function scrollToSection() {
-        // get section id
-        const menulinkId = menulink.id;
-        const sectionId = menulinkId.slice(0, menulinkId.indexOf('-'));
+  menulink.addEventListener('click', function scrollToSection () {
+    // get section id
+    const menulinkId = menulink.id;
+    const sectionId = menulinkId.slice(0, menulinkId.indexOf('-'));
 
-        // determine vertical scroll position
-        const yPos = document.querySelector(`#${sectionId}`).getBoundingClientRect().top;
-        const yOffset = window.pageYOffset;
-        window.scrollTo(0, pageYOffset + yPos - navbarHeight);
-    })
+    // determine vertical scroll position
+    const yPos = document.querySelector(`#${sectionId}`).getBoundingClientRect().top;
+    const yOffset = window.pageYOffset;
+    window.scrollTo(0, yOffset + yPos - navbarHeight);
+  })
 }
 
 // Set active section on scroll
-window.addEventListener('scroll', function() {
-    setActive();
+window.addEventListener('scroll', function () {
+  setActive();
 })
